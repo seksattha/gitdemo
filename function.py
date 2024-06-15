6b042f0ffa887d9554e8434c755511708f3bff99
seksattha <seksattha@gmail.com>
2024-06-15 19:56:31 +0700

    add demo2 function

--- function.py
+++ function.py
@@ -1,5 +1,15 @@
+import sqlite3
 def demo1():
     for i in range(1,10):
         print(i)
 
 demo1()
+
+def demo2():
+    try:
+        with sqlite3.connect("test.db") as con:
+            sql_cmd = """
+                create table t1 
+            """
+    except Exception as e:
+        print(f'error {e}')
